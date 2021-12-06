2d7a8274f1705ade53961b92f72e62d62848e7d4
Ilolo Izu <iloloizu97@gmail.com>
2021-12-06 08:31:55 -0600

    commit tree 0d70f690abd302874b54f15527bb3fd1375dd45e

---
+++ script.js
@@ -0,0 +1,18 @@
+// const clicker = document.getElementById("ME");
+
+// function clickAlert() {
+ //   alert('nice smile, huh!?');
+//  }
+  
+//  clicker.addEventListener('click', clickAlert);
+
+
+// const clicked = document.getElementById("trackstar");
+
+ // function clickAlert1() {
+ //     alert('he a runner he a trackstar!');
+ //   }
+    
+ //   clicked.addEventListener('click', clickAlert1);
+
+
