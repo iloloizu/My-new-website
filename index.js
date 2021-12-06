6c70ba4dab7aa73d2d0188f20cb4629901f4358c
Ilolo Izu <iloloizu97@gmail.com>
2021-12-06 08:33:46 -0600

    Added Javascript ClickAlert

---
+++ index.js
@@ -0,0 +1,17 @@
+const clicker = document.getElementById("ME");
+
+function clickAlert() {
+    alert('Nice smile right?!');
+  }
+  
+  clicker.addEventListener('click', clickAlert);
+  
+
+const clickMe = document.getElementById("trackstar");
+
+function clickAlertAgain() {
+    alert('he a runner he a trackstar!');
+  }
+  
+clickMe.addEventListener('click', clickAlertAgain);
+
